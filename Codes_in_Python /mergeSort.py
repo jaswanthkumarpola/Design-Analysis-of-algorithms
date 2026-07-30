246affb4cefe30bf68bcb0f53546012a4082be51
jaswanthkumarpola <jaswanthpola12@gmail.com>
2026-07-30 16:42:37 +0530

    Finish mergeSort algo

---
+++ Codes_in_Python /mergeSort.py
@@ -0,0 +1,28 @@
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
+    else :
+        mid = len(arr)//2
+        left = mergeSort(arr[:mid])
+        right = mergeSort(arr[mid:])
+        return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i=j=0
+
+    while i<len(left) and j<len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i+=1
+        else:
+            result.append(right[j])
+            j+=1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result   
+
+
+arr = list(map(int, input("Enter the elements for the array :").split()))
+print("The sorted array :",mergeSort(arr))
